458f0fc2ba582e91a194c359d576eb3272729ee4
Jolitaviso <jolitavinter@gmail.com>
2024-02-28 13:10:52 +0200

    100val dirbau, jokio pokyčio

--- blog/communication/admin.py
+++ blog/communication/admin.py
@@ -1,3 +1,88 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from . import models
 
-# Register your models here.
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'total_communications', 'owner', 'created_at', 'recent_communications']
+    list_display_links = ['id', 'name']
+    list_filter = ['owner', 'created_at']
+    search_fields = ['name', 'owner']
+    readonly_fields = ['updated_at']
+    fields = (
+        (None, {
+            "fields": (
+                'name', 'owner',
+            ),
+        }),
+        (_('temporal tracking').title(), {
+            'fields': ('created_at', 'updated_at'),
+        }),
+    )
+ 
+    autocomplete_fields = ['owner']
+    
+    def total_communications(self, obj: models.Blog):
+        return obj.communications.count()
+    total_communications.short_description = _('total communications')
+
+    def recent_communications(self, obj: models.Blog):
+        return "; ".join(obj.communications.order_by('-created_at').values_list('name', flat=True)[:3])
+    recent_communications.short_description = _('recent communications')
+
+
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'blog', 'owner', 'created_at']
+    list_filter =  ['created_at']
+    search_fields = ['name', 'description', 'blog__name', 'owner__last_name', 'owner__username']
+    list_editable = ['owner', 'blog']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    autocomplete_fields = ['blog', 'owner']
+    fieldsets = (
+        (_("general").title(), {
+            "fields": (
+                ('name'),
+                'description',
+            ),
+        }),
+        (_("ownership").title(), {
+            "fields": (
+                ('owner', 'blog'),
+            ),
+        }),
+        (_("temporal tracking").title(), {
+            "fields": (
+                ('created_at', 'updated_at', 'id'),
+            ),
+        }),
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'created_at']
+    list_filter =  ['created_at', 'owner']
+    search_fields = ['title', 'note', 'owner__last_name', 'owner__username']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+    fieldsets = (
+        (_("general").title(), {
+            "fields": (
+                'title',
+                'note',
+            ),
+        }),
+        (_("ownership").title(), {
+            "fields": (
+                'owner',
+            ),
+        }),
+        (_("temporal tracking").title(), {
+            "fields": (
+                ('created_at', 'updated_at', 'id'),
+            ),
+        }),
+    )
+    
+    
+admin.site.register(models.Blog)
+admin.site.register(models.Communication)
+admin.site.register(models.Comment)
--- blog/communication/models.py
+++ blog/communication/models.py
@@ -1,3 +1,90 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Blog(models.Model):
+    name = models.CharField(_("name"), max_length=200, db_index=True)
+    description = models.TextField(_("description"), blank=True, max_length=5000)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
+    owner = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        verbose_name=_("owner"), 
+        related_name='blogs',
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("blog")
+        verbose_name_plural = _("blogs")
+        ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"pk": self.pk})
+
+
+class Communication(models.Model):
+    name = models.CharField(_("name"), max_length=200, db_index=True)
+    description = models.TextField(_("description"), blank=True, max_length=10000)
+    blog = models.ForeignKey(
+        Blog,
+        on_delete=models.CASCADE, 
+        verbose_name=_("blog"), 
+        related_name='communications',
+    )
+    owner = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        verbose_name=_("owner"), 
+        related_name='communications',
+    )
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
+
+    comments = models.ManyToManyField(
+        'Comment',
+        verbose_name=_("comments"),
+        related_name='communications',
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("communication")
+        verbose_name_plural = _("communications")
+        ordering = ['-created_at']
+
+    def get_absolute_url(self):
+        return reverse("communication_detail", kwargs={"pk": self.pk})
+    
+class Comment(models.Model):
+    title = models.CharField(_("title"), max_length=200, db_index=True)
+    note = models.TextField(_("note"), blank=True, max_length=10000)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
+    owner = models.ForeignKey(
+        get_user_model(), 
+        on_delete=models.CASCADE, 
+        verbose_name=_("owner"), 
+        related_name='comments',
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _("comment")
+        verbose_name_plural = _("comments")
+        ordering = ['-created_at']
+
+    def get_absolute_url(self):
+        return reverse("comment_detail", kwargs={"pk": self.pk})
+    
+    
