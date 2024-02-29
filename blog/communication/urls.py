84f64e35f45d611643679f618bd080edf063ad2a
Jolitaviso <jolitavinter@gmail.com>
2024-02-29 02:34:06 +0200

    Blogas sutvarkytas +-

--- blog/communication/urls.py
+++ blog/communication/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('communication/', views.communication_list, name='communication_list'),
     path('communication/<int:pk>/', views.communication_detail, name='communication_detail'),
+    path('blogs/', views.BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('blogs/create/', views.BlogCreateView.as_view(), name='blog_create'),  
+    path('blog/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='blog_update'),  
+    path('blog/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blog/communication/views.py
+++ blog/communication/views.py
@@ -1,13 +1,71 @@
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpRequest, HttpResponse
+from django.views import generic
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
 from . import models
 
+class BlogListView(generic.ListView):
+    model = models.Blog
+    template_name = 'communication/blog_list.html'
+
+
+class BlogDetailView(generic.DetailView):
+    model = models.Blog
+    template_name = 'communication/blog_detail.html'
+    
+    
+class BlogCreateView(LoginRequiredMixin, generic.CreateView):
+    model = models.Blog
+    template_name = 'communication/blog_create.html'
+    fields = ('name', )
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('blog created successfully').capitalize())
+        return reverse('blog_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class BlogUpdateView(
+        LoginRequiredMixin, 
+        UserPassesTestMixin, 
+        generic.UpdateView
+    ):
+    model = models.Blog
+    template_name = 'communication/blog_update.html'
+    fields = ('name', )
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('blog updated successfully').capitalize())
+        return reverse('blog_list')
+
+    def test_func(self) -> bool | None:
+        return self.get_object().owner == self.request.user
+    
+class BlogDeleteView(
+        LoginRequiredMixin, 
+        UserPassesTestMixin, 
+        generic.DeleteView
+    ):
+    model = models.Blog
+    template_name = 'communication/blog_delete.html'
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, _('blog deleted successfully').capitalize())
+        return reverse('blog_list')
+
+    def test_func(self) -> bool | None:
+        return self.get_object().owner == self.request.user
+    
+    
 def index(request: HttpRequest) -> HttpResponse:
     context = {
         'blogs_count'.capitalize: models.Blog.objects.count(),
-        'comunnications_count'.capitalize: models.Communication.objects.count(),
+        'communnications_count'.capitalize: models.Communication.objects.count(),
         'users_count'.capitalize: models.get_user_model().objects.count(),
         'comments_count'.capitalize: models.Comment.objects.count(),
     }
